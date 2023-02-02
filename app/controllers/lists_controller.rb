d53a347b77f5485032dc882dfc6916abd9c7f0d8
EC2 Default User <ec2-user@ip-172-31-10-103.ap-northeast-1.compute.internal>
2023-02-02 08:06:10 +0000

    #1RSpecによるカリキュラム完了しました

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,21 +24,21 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     flash[:notice] = "編集しました!!"
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
     flash[:notice] = "削除しました。"
-    redirect_to "/lists"
+    redirect_to lists_path
   end
-  
+
   private
   def list_params
     params.require(:list).permit(:title,:body,:image)
