27c4c51ab8c82b8e3c915fc11a3affbf8839bc79
EC2 Default User <ec2-user@ip-172-31-10-103.ap-northeast-1.compute.internal>
2023-02-03 06:03:38 +0000

    インデント修正

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -8,6 +8,7 @@ describe '投稿のテスト' do
     before do
       visit root_path
     end
+
     context '表示の確認' do
       it 'トップ画面(root_path)に初めてのtop画面制作が表示されているか' do
         expect(page).to have_content '初めてのtop画面制作'
@@ -17,10 +18,12 @@ describe '投稿のテスト' do
       end
     end
   end
+
   describe '投稿画面(new_list_path)のテスト' do
      before do
        visit new_list_path
      end
+
       context '表示の確認' do
        it 'new_list_pathが"/lists/new"であるか' do
         expect(current_path).to eq('/lists/new')
@@ -29,6 +32,7 @@ describe '投稿のテスト' do
         expect(page).to have_button '投稿'
       end
      end
+
       context '投稿処理のテスト' do
         it '投稿後のリダイレクト先は正しいか' do
         fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
@@ -38,6 +42,7 @@ describe '投稿のテスト' do
        end
       end
   end
+
   describe '一覧画面(lists_path)のテスト' do
     before do
       visit lists_path
@@ -49,10 +54,12 @@ describe '投稿のテスト' do
       end
     end
   end
+
   describe '詳細画面のテスト' do
     before do
       visit list_path(list)
     end
+
     context '表示の確認' do
       it '削除リンクが存在しているか' do
         expect(page).to have_link '削除'
@@ -61,6 +68,7 @@ describe '投稿のテスト' do
         expect(page).to have_link '編集'
       end
     end
+
     context 'リンクの遷移先の確認' do
       it '編集の遷移先は編集画面か' do
         edit_link = find_all('a')[3]
@@ -68,16 +76,19 @@ describe '投稿のテスト' do
         expect(current_path).to eq('/lists/' + list.id.to_s + '/edit')
       end
     end
+
     context 'list削除のテスト' do
       it 'listの削除' do
         expect{ list.destroy }.to change{ List.count }.by(-1)
       end
     end
   end
+
   describe '編集画面のテスト' do
     before do
       visit edit_list_path(list)
     end
+
      context '表示の確認' do
        it '編集前のタイトルと本文がフォームに表示（セット）されている' do
         expect(page).to have_field 'list[title]', with: list.title
@@ -87,6 +98,7 @@ describe '投稿のテスト' do
          expect(page).to have_button '保存'
        end
      end
+
      context '更新処理に関するテスト' do
        it '更新後のリダイレクト先は正しいか' do
         fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
